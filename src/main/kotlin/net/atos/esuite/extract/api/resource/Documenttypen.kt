99711aa631ad08c2da28f9077bd04633f65d23ed
Andy Verberne <andy.verberne@atos.net>
2025-12-09 13:17:53 +0100

    Add `Documenttypen` resource, repository, converters, HTTP client tests, OpenAPI schema updates

--- src/main/kotlin/net/atos/esuite/extract/api/resource/Documenttypen.kt
+++ src/main/kotlin/net/atos/esuite/extract/api/resource/Documenttypen.kt
@@ -6,36 +6,36 @@ import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
-import net.atos.esuite.extract.api.converter.toDocumentStatus
+import net.atos.esuite.extract.api.converter.toDocumenttype
 import net.atos.esuite.extract.api.converter.toPage
 import net.atos.esuite.extract.api.model.shared.BladerParameters
 import net.atos.esuite.extract.api.model.shared.Fout
 import net.atos.esuite.extract.api.model.shared.Results
 import net.atos.esuite.extract.api.model.shared.ValidatieFouten
-import net.atos.esuite.extract.db.zakenmagazijn.repository.DocumentStatusRepository
+import net.atos.esuite.extract.db.zakenmagazijn.repository.DocumentTypeRepository
 import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.media.Content
 import org.eclipse.microprofile.openapi.annotations.media.Schema
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
 
-@Path("documentstatussen")
+@Path("documenttypen")
 @APIResponse(responseCode = "401", description = "Unauthorized")
 @APIResponse(
     responseCode = "500", description = "Internal Server Error",
     content = [Content(schema = Schema(implementation = Fout::class))]
 )
-class DocumentStatussen(private val documentStatusRepository: DocumentStatusRepository) {
+class Documenttypen(private val documentTypeRepository: DocumentTypeRepository) {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(operationId = "documentstatus_list", summary = "Lijst van document statussen opvragen")
+    @Operation(operationId = "documenttype_list", summary = "Lijst van documenttypen opvragen")
     @APIResponse(responseCode = "200", description = "OK")
     @APIResponse(
         responseCode = "400", description = "Bad Request",
         content = [Content(schema = Schema(implementation = ValidatieFouten::class))]
     )
-    fun documentstatusList(@BeanParam @Valid bladerParameters: BladerParameters) =
-        with(documentStatusRepository.findAll().page(bladerParameters.toPage())) {
-            Results(list().map { it.toDocumentStatus() }, count(), hasPreviousPage(), hasNextPage())
+    fun documenttypeList(@BeanParam @Valid bladerParameters: BladerParameters) =
+        with(documentTypeRepository.findAll().page(bladerParameters.toPage())) {
+            Results(list().map { it.toDocumenttype() }, count(), hasPreviousPage(), hasNextPage())
         }
 }
